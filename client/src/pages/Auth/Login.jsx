3466c85e48fde627daeaa726314032879faed1cb
Pleian Andrei Mihai <69640908+Andi-Pleian@users.noreply.github.com>
2025-01-25 00:18:28 +0200

    Added login and register pages
    Login and register pages implemented but without backend functionality

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styles from './Login.module.css'
+import login from '../../assets/login.png'
+import {Button, Input} from 'antd'
+import { Link } from 'react-router'
 
 function Login() {
+  const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+
+  const handleSubmit = ()=> {
+    console.log("Login");
+  }
+
   return (
-    <div>Login</div>
+    <div className={styles.login__card}>
+      <img src={login} alt=".."/>
+
+      <h4>Login</h4>
+
+      <div className={styles.input__wrapper}>
+        <Input 
+        placeholder="Username" 
+        value={username} 
+        onChange={(e)=>setUsername(e.target.value)}/>
+
+        <Input.Password
+        placeholder="Password" 
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}/>
+      </div>
+
+      <div className={styles.input__info}>
+        New Use? <Link to="/register"> Register </Link>
+      </div>
+
+      <Button type="primary" size="large" disabled={!username || !password} onClick={handleSubmit}>Login</Button>
+    </div>
   )
 }
+
+// button disabled option tells the button when it should be disabled (line 33)
 
 export default Login
--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styles from './Login.module.css'
+import login from '../../assets/login.png'
+import {Button, Input} from 'antd'
+import { Link } from 'react-router'
 
 function Register() {
+  const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const handleSubmit = ()=> {
+    console.log("register");
+  }
+
   return (
-    <div>Register</div>
+    <div className={styles.login__card}>
+      <img src={login} alt=".."/>
+
+      <h4>Register</h4>
+
+      <div className={styles.input__inline__wrapper}>
+        <Input 
+        placeholder="First Name" 
+        value={firstName} 
+        onChange={(e)=>setFirstName(e.target.value)}/>
+
+        <Input 
+        placeholder="Last Name" 
+        style={{marginLeft:'10px'}}
+        value={lastName} 
+        onChange={(e)=>setLastName(e.target.value)}/>
+      </div>
+
+      <div className={styles.input__wrapper}>
+        <Input 
+        placeholder="Username" 
+        value={username} 
+        onChange={(e)=>setUsername(e.target.value)}/>
+      </div>
+
+      <div className={styles.input__wrapper}>
+        <Input.Password
+        placeholder="Password" 
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}/>
+      </div>
+
+      <div className={styles.input__info}>
+        Existing user? <Link to="/login"> Login </Link>
+      </div>
+
+      <Button type="primary" size="large" disabled={!username || !password} onClick={handleSubmit}>Register</Button>
+    </div>
   )
 }
+
+// button disabled option tells the button when it should be disabled (line 33)
 
 export default Register
